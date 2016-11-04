0138760f226667e53b87490fe8162d519b0fbc1f
Sergey Stelmakh <stelmakhserg@gmail.com>
2016-11-04 12:02:43 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+(function() {
+    new WOW().init();
+
+
+    $('.menu-link').click(function() {
+        var scrollSpeed = 1000; // Default scrolling speed
+        console.log(this.hash);
+        if (this.hash == '#features-section') {
+            scrollSpeed = 1500;
+        } else if (this.hash == '#pricing-section') {
+            scrollSpeed = 2000;
+        } else if (this.hash == '#newsletter-section') {
+            scrollSpeed = 2500;
+        }
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, scrollSpeed);
+                return false;
+            }
+        }
+    });
+
+})();
